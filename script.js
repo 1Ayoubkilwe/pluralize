cc5e49bf6717efd67c729ec126d6752e8280b4af
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-08 16:56:51 +0300

    answers

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function pluralize(words) {
+    let wordCount = {};
+    
+    
+    words.forEach(word => {
+      wordCount[word] = (wordCount[word] || 0) + 1;
+    });
+    
+    
+    return [...new Set(words)].map(word => wordCount[word] > 1 ? word + 's' : word);
+  }
+  
+  
+  console.log(pluralize(["cat", "dog", "cat", "mouse"])); // ["cats", "dog", "mouse"]
+  console.log(pluralize(["car", "car", "car"])); // ["cars"]
+  
